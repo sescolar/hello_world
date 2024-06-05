8caaaaf0b5994055f2c5ef044af636ff264143f1
Soledad Escolar <soledad.escolar@gmail.com>
2024-06-05 06:12:32 +0200

    __init__.py updated

--- __init__.py
+++ __init__.py
@@ -3,8 +3,8 @@ DOMAIN = "hello_world"
 #Selecciona uno de los dos métodos y comenta el que no uses
 def setup(hass, config):
     hass.states.async_set("hello_state.world", "all")
-    return true
+    return True 
 
 #async def async_setup(hass, config):
 #    hass.states.async_set("hello_state.world", "all")
-#    return true
+#    return True
